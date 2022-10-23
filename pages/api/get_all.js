5c03a6eda9bdf4476351429dbb96d7562110b137
BrianBitopia <brian@bitopia.digital>
2022-10-23 17:23:45 -0300

    first commit

---
+++ pages/api/get_all.js
@@ -0,0 +1,18 @@
+import clientPromise from "../../lib/mongodb";
+
+export default async (req, res) => {
+    try {
+        const client = await clientPromise;
+        const db = client.db("test");
+
+        const movies = await db
+            .collection("datas")
+            .find({})
+            .limit(10)
+            .toArray();
+
+        res.json(movies);
+    } catch (e) {
+        console.error(e);
+    }
+};
